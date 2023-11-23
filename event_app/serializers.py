30b9c7a59ed5b3742f68a6d13e93d484ae08af79
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-11-23 13:34:04 +0100

    implementation of ModelViewsets, serialiasers class and define urls

---
+++ event_app/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Event, Participant,Address,Location, Reservation,Ticket
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Address
+        fields='__all__'
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Location
+        fields='__all__'
+
+        
+class EventSerializer(serializers.ModelSerializer):
+    location=LocationSerializer()
+    class Meta:
+        model=Event
+        fields='__all__'
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    participation=EventSerializer(many=True)
+    class Meta:
+        model=Participant
+        fields='__all__'
+
+class  ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Reservation
+        fields='__all__'
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Ticket
+        fields='__all__' 
--- event_app/views.py
+++ event_app/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Address, Event, Location, Participant, Reservation, Ticket
+from .serializers import (
+    AddressSerializer,
+    EventSerializer,
+    LocationSerializer,
+    ParticipantSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+)
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class ParticipantViewSet(viewsets.ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+    http_method_names = [
+        "GET",
+        "POST",
+        "PUT",
+    ]
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
